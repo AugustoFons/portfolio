0613ce579c018e3841bfa73f9e724a0af218666e
augustoFons <augustofonsdev@gmail.com>
2023-08-08 20:48:11 -0300

    responsive about

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -17,14 +17,14 @@ const AboutMe = () => {
     return (
         <section className="lg:flex lg:items-center lg:justify-center">
             <div className="w-full lg:w-3/4 flex flex-col items-center">
-                <h1 id="sobreMi" className="fuenteTitulo text-5xl text-center">Sobre Mi:</h1>
+                <h1 id="sobreMi" className="fuenteTitulo text-5xl text-center mt-4">Sobre Mi:</h1>
                 <Image 
                     src='/line.png'
                     width={100}
                     height={80}
                     alt="line"
                     />
-                <div className="pt-0 mt-2 animacion hover:pb-5" style={{
+                <div className="animacion hover:pt-8 hover:pb-5" style={{
                         backgroundImage: `url('/fondo1.png')`,
                         background: '100% 91% no-repeat',
                     }}>
@@ -33,11 +33,11 @@ const AboutMe = () => {
                         height={100}
                         src='/mifoto.png'
                         alt="Mi Foto"
-                        className="rounded-full pt-6 pb-2 lg:pb-0 lg:hidden hover:scale-125 hover:backdrop-blur-sm"
+                        className="rounded-full pt-3 pb-2 lg:pb-0 lg:hidden hover:scale-125 hover:backdrop-blur-sm"
                         quality="100"
                         />  
                 </div>
-                <p className="justify-center indent-1 py- text-base lg:text-lg hyphens-auto fuenteTexto">
+                <p className="justify-center indent-1 py-2 text-base lg:text-lg hyphens-auto fuenteTexto">
                 ¡Hola! me llamo Augusto Fons, soy desarrollador web Full Stack, vivo en La ciudad de La Plata, Argentina. Apasionado por la programación y la creación de soluciones digitales. En este portfolio me gustaría compartirles varios de los proyectos en los que he estado trabajando, y con los que aprendo día a día. Actualmente me interesa ser parte de proyectos y oportunidades en las cuales pueda potenciar, desarrollar y aplicar mis conocimientos y habilidades. Para saber más sobre mí, pueden descargar mi CV o ver mis redes que dejaré en la sección de contacto.
                 </p>
                 

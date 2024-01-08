7ee3b55d14a91f851e7059794f0aa7cda0886bb4
AugustoFons <augustofonsdev@gmail.com>
2024-01-07 21:20:06 -0300

    cambio schema

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,15 @@
+import { g, config } from '@grafbase/sdk'
+
+const Project = g.model('Project', {
+  title: g.string(),
+  description: g.string(),
+  image: g.url(),
+  siteUrl: g.url(),
+  gitHub: g.url(),
+  category: g.string().search(),
+  order: g.int()
+})
+
+export default config({
+  schema: g
+})
